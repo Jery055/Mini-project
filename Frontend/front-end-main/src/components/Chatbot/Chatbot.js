4bbbdd365d3f3307c6b6ec095da62d7497ac2408
Devika Biju <u2209017@rajagiri.edu.in>
2024-11-06 22:55:57 +0530

    try

--- Frontend/front-end-main/src/components/Chatbot/Chatbot.js
+++ Frontend/front-end-main/src/components/Chatbot/Chatbot.js
@@ -29,7 +29,7 @@ const Chatbot = () => {
     setUserInput("");
 
     try {
-      const response = await fetch("/chat", {
+      const response = await fetch("Backend/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt: userMessage }),
