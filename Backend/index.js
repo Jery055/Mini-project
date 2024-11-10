2ff44fd5065dc6237aa0d91916abac46203eba78
Jery <u2209028@rajagiri.edu.in>
2024-11-10 14:10:13 +0530

    update cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 require('dotenv').config();
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
